514f0593c5b2ebe3ea7ce8f29453f09b59e7c074
olympiawoj <owojcik7@gmail.com>
2021-01-25 10:18:44 -0500

    1. Time and Start

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function padTime(time){
+  return time.toString().padStart(2, '0')
+}
+export default function App() {
+  const [title, setTitle] = useState('Let the countdown begin!!!!')
+  const [timeLeft, setTimeLeft] = useState(10) // 25 minutes * 60 seconds
+  
+  function startTimer(){
+    setInterval(()=>{
+      setTimeLeft(timeLeft => {
+        if (timeLeft >= 1) return timeLeft - 1;
+
+        // reset the timer
+        return 0;
+      });
+    }, 1000) // 1000 ms, 1 sec
+  }
+
+  const minutes = padTime(Math.floor(timeLeft / 60)) // If 1 minute, this will say 01
+  const seconds = padTime(timeLeft - (minutes * 60)) // w/e minutes are left, subtract from overall time & that's what our seconds are left
+ 
+
+  return (
+    <div className="app">
+      <h2>{title}</h2>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        <button onClick={startTimer}>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
+}
